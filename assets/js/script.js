759de24bf448bda57b047e6ca2dc30271090b8f9
Lore Vincit <lore@dagobah.Endor>
2021-07-17 22:13:34 -0400

    se hacen arreglos menores, se añade popovers y modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
     $('[data-toggle="popover"]').popover()
-    $(​ "a"​ ).on(​ 'click'​ , ​ function​ (event) {
-        if​ (​ this​ .hash !== ​ ""​ ) {
-            event.preventDefault();
-            var​ hash = ​ this​ .hash;
-            $(​ 'html, body'​ ).animate({
-                scrollTop: $(hash).offset().top
-                }, ​ 800​ , ​ function​ (){
-                    window​ .location.hash = hash;
-    });
-    } ​ // End if
-    });
-});
+  })
